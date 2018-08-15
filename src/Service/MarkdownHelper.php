dd71ed061a70f52fa99ec2cbbcf24364c695c4e8
developer <dima3141592@gmail.com>
2018-08-15 09:00:46 +0300

    articleAdminController and twigExtension and knp-time-bundle

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -30,22 +30,19 @@ class MarkdownHelper
 
     public function parse(string $source): string
     {
+        if (stripos($source, 'bacon') !== false) {
+            $this->logger->info('They are talking about bacon again!');
+        }
+
+        // skip caching entirely in debug
+        if ($this->isDebug) {
+            return $this->markdown->transform($source);
+        }
+
         $item = $this->cache->getItem('markdown_'.md5($source));
         if (!$item->isHit()) {
             $item->set($this->markdown->transform($source));
             $this->cache->save($item);
-        }
-
-        if ($this->isDebug) {
-            return $this->markdown->transform($source);
-        }
-
-        if ($this->isDebug) {
-            return $this->markdown->transform($source);
-        }
-
-        if (stripos($source, 'bacon') !== false) {
-            $this->logger->info('They are talking about bacon again!');
         }
 
         return $item->get();
