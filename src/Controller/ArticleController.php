f0dfb26b1723a57b03c7927ff36497a67c3c89bb
developer <dima3141592@gmail.com>
2018-08-15 12:51:12 +0300

    dataFixtures

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -68,13 +68,18 @@ class ArticleController extends AbstractController
 
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     * @param Article $article
+     * @param LoggerInterface $logger
+     * @param EntityManagerInterface $em
+     * @return Response
      */
-    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    public function toggleArticleHeart(Article $article, LoggerInterface $logger, EntityManagerInterface $em)
     {
-        // TODO - actually heart/unheart the article!
+        $article->incrementHeartCount();
+        $em->flush();
 
         $logger->info('Article is being hearted!');
 
-        return new JsonResponse(['hearts' => rand(5, 100)]);
+        return new JsonResponse(['hearts' => $article->getHeartCount()]);
     }
 }
